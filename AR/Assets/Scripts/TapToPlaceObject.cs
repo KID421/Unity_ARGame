9a723a921220d69aecfa332f5b9056b4dab48526
KID <50666207+KID421@users.noreply.github.com>
2020-05-14 11:57:34 +0800

    完成放置物件

---
+++ AR/Assets/Scripts/TapToPlaceObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class TapToPlaceObject : MonoBehaviour
+{
+    public GameObject dragon;
+
+    private GameObject spawnDragon;
+    private ARRaycastManager arRaycastManager;
+    private Vector2 touchPosition;
+
+    public List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Start()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    private void PlaceObject()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            touchPosition = Input.GetTouch(0).position;
+
+            if (arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+
+                spawnDragon = Instantiate(dragon, hitPose.position, Quaternion.identity);
+                Vector3 look = transform.position;
+                look.y = spawnDragon.transform.position.y;
+                spawnDragon.transform.LookAt(look);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        PlaceObject();
+    }
+}
