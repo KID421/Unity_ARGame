ba0ac89fe333541d2653012f384facdcc56cd70d
KID <50666207+KID421@users.noreply.github.com>
2020-05-14 13:02:51 +0800

    完成

--- AR/Assets/Scripts/TapToPlaceObject.cs
+++ AR/Assets/Scripts/TapToPlaceObject.cs
@@ -2,14 +2,12 @@
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
-using System;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class TapToPlaceObject : MonoBehaviour
 {
     public GameObject dragon;
 
-    private GameObject spawnDragon;
     private ARRaycastManager arRaycastManager;
     private Vector2 touchPosition;
 
@@ -24,13 +22,13 @@ public class TapToPlaceObject : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            touchPosition = Input.GetTouch(0).position;
+            touchPosition = Input.mousePosition;
 
             if (arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
             {
                 Pose hitPose = hits[0].pose;
 
-                spawnDragon = Instantiate(dragon, hitPose.position, Quaternion.identity);
+                GameObject spawnDragon = Instantiate(dragon, hitPose.position, Quaternion.identity);
                 Vector3 look = transform.position;
                 look.y = spawnDragon.transform.position.y;
                 spawnDragon.transform.LookAt(look);
